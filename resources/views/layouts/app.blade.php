1ff157670001040743e433575258234d5425e824
ajlsemana <ajlsemana@gmail.com>
2016-08-03 12:11:43 +0400

    GUI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,21 +60,11 @@
            <div class="subnavbar-inner">
              <div class="container">
                <ul class="mainnav">
-                 <li class="active"><a href="index.html"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
-                 <li><a href="reports.html"><i class="icon-list-alt"></i><span>Reports</span> </a> </li>
-                 <li><a href="guidely.html"><i class="icon-facetime-video"></i><span>App Tour</span> </a></li>
-                 <li><a href="charts.html"><i class="icon-bar-chart"></i><span>Charts</span> </a> </li>
-                 <li><a href="shortcodes.html"><i class="icon-code"></i><span>Shortcodes</span> </a> </li>
-                 <li class="dropdown"><a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-long-arrow-down"></i><span>Drops</span> <b class="caret"></b></a>
-                   <ul class="dropdown-menu">
-                     <li><a href="icons.html">Icons</a></li>
-                     <li><a href="faq.html">FAQ</a></li>
-                     <li><a href="pricing.html">Pricing Plans</a></li>
-                     <li><a href="login.html">Login</a></li>
-                     <li><a href="signup.html">Signup</a></li>
-                     <li><a href="error.html">404</a></li>
-                   </ul>
-                 </li>
+                 <li class="active"><a href="#"><i class="icon-home"></i><span>Home</span> </a> </li>
+                 <li><a href="#"><i class="icon-group"></i><span>Workforce</span> </a> </li>
+                 <li><a href="#"><i class="icon-user"></i><span>Agent Profiles</span> </a></li>
+                 <li><a href="#"><i class="icon-sitemap"></i><span>Departments</span> </a> </li>
+                 <li><a href="#"><i class="icon-comments-alt"></i><span>Announcements</span> </a> </li>                 
                </ul>
              </div>    
            </div>  
