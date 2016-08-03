1d252a425a9ac8c421a5a058cab0c8d6d3de661d
ajlsemana <ajlsemana@gmail.com>
2016-08-03 10:27:19 +0400

    Title Modified

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>EMAAR - HR Workforce System</title>
+    <title>EMAAR - HR Workforce Tool</title>
     <link rel="shortcut icon" href="{!! asset('favicon.ico') !!}">
 
     <link rel="stylesheet" href="{!! asset('public/css/bootstrap.min.css') !!}">
